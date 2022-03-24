0f1c23f71bd5ab6b8a606b1ab802bb12983b3483
svv <vvsh1978@mail.ru>
2022-03-24 21:27:38 +0500

    feat: add variables, alert, console.log to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'Lesson01';
+const screens = [];
+let screenPrice = 1000;
+const rollback = 0.3;
+let fullPrice = screenPrice * (1 + rollback);
+let adaptive;
+
+alert('Hello, Сергей! Нажми ОК');
+console.log('lesson01');
