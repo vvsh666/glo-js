dc08e1bb220c419f4ce28ef7ba5941a4cd8b0333
svv <vvsh1978@mail.ru>
2022-03-29 21:26:30 +0500

    feat: make lesson04

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const title = prompt('Как называется ваш проект?', 'JS Project');
+let title = prompt('Как называется ваш проект?', '   КальКуЛятор ВеРстки  ');
 const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 const screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
 const adaptive = confirm('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?', 'Первый');
+const service1 = prompt('Какой дополнительный тип услуги нужен?', 'Связка с WordPress');
 const servicePrice1 = +prompt('Сколько это будет стоить?', 10000);
-const service2 = prompt('Какой дополнительный тип услуги нужен?', 'Второй');
+const service2 = prompt('Какой дополнительный тип услуги нужен?', 'Хостинг');
 const servicePrice2 = +prompt('Сколько это будет стоить?', 5000);
 const rollback = 25;
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.floor(fullPrice * (100 - rollback) / 100);
+
+let allServicePrice, fullPrice, servicePercentPrice;
+
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable)
@@ -28,17 +29,37 @@ const getRollbackMessage = function (price) {
   }
 }
 
+const getAllServicePrices = function (servicePrice1, servicePrice2) {
+  return servicePrice1 + servicePrice2
+}
+
+function getFullPrice(screenPrice, allServicePrice) {
+  return screenPrice + allServicePrice
+}
+
+const getTitle = function (title) {
+  let titleLower = title.trim().toLowerCase();
+  if (!titleLower) return titleLower;
+  let resultTitle = titleLower[0].toUpperCase() + titleLower.slice(1);
+  return resultTitle
+}
+
+const getServicePercentPrices = function (fullPrice, rollback) {
+  return Math.floor(fullPrice * (100 - rollback) / 100);
+}
+
+
+title = getTitle(title);
+
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
+allServicePrice = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrice);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+
+console.log(screens);
 console.log(getRollbackMessage(fullPrice));
-console.log('title: ', typeof title);
-console.log('fullPrice: ', typeof fullPrice);
-console.log('adaptive: ', typeof adaptive);
-console.log(screens.length)
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
-console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
-console.log(servicePercentPrice);
+console.log(servicePercentPrice)
