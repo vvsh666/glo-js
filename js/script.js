ca7b936bf28c5e488119129bcfaf671980efbcb4
svv <vvsh1978@mail.ru>
2022-04-05 18:22:34 +0500

    feat: make lesson09

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const title = document.getElementsByTagName('h1')[0];
+const buttons = document.getElementsByClassName('handler_btn');
+const screenBtn = document.querySelector('.screen-btn');
+const percentItems = document.querySelectorAll('.other-items.percent');
+const numberItems = document.querySelectorAll('.other-items.number');
+const rangeInput = document.querySelector('.rollback input');
+const rangeValue = document.querySelector('.rollback .range-value');
+
+const totalInputs = document.getElementsByClassName('total-input');
+const totalInput = totalInputs[0];
+const totalCountInput = totalInputs[1];
+const totalCountOtherInput = totalInputs[2];
+const totalFullCountInput = totalInputs[3];
+const totalCountRollbackInput = totalInputs[4];
+
+let screens = document.querySelectorAll('.screen');
+
 const appData = {
   title: '',
   screens: [],
